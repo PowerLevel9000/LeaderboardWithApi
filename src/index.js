d37bf4e19cdfebc0ac4156c8dc701c699757605b
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-10 07:29:34 +0530

    fixing linters

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import joke from './joke.js';
 import './styles/main.scss';
-import add from './add';
-console.log(joke());
+import add from './add.js';
 
 const submitBtn = document.getElementById('submit');
 
-const sendingData = async(event) => {
-  event.preventDefault()
+const sendingData = async (event) => {
+  event.preventDefault();
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
   await add(name, score);
-}
-submitBtn.addEventListener('click', sendingData)
+};
+submitBtn.addEventListener('click', sendingData);
