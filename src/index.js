9fd377eeb31f9e2cdbb94f11c1ed4d97075c4291
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-10 23:48:25 +0530

    animated styling

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ const disableButton = () => {
 };
 const sendingData = async (event) => {
   event.preventDefault();
+
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
+  const error = document.querySelector('small')
+  if (name.length < 20 || !isNaN(score) || score < 1000000) {
+    error.innerHTML = "please enter valid score or name";
+  }
   await add(name, score);
   window.location.reload();
 };
@@ -26,7 +31,7 @@ refreshBtn.addEventListener('click', () => {
 const leaderboard = document.querySelector('.leaderboard');
 const displayScores = async () => {
   const waitingData = await getData();
-  const descendingData = waitingData.result.sort((a, b) => b.score - a.score);
+  const descendingData = waitingData.result.sort((a, b) => b.score - a.score).filter((a) => a.score <= 1000000000 && a.user.length <= 20);
   descendingData.forEach((result) => {
     leaderboard.innerHTML += `
     <div class="score">${result.user.toUpperCase()}  <span>${result.score}</span></div>
