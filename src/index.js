5d9479157b075a44ad4fb76bd5d4101699ea305e
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-11 00:44:51 +0530

    add contact section

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 import './styles/main.scss';
 import { add, getData } from './add.js';
-
+//  for now lets leave the contact
 const submitBtn = document.getElementById('submit');
+const navLeaderboard = document.getElementById('leaderBoard');
+const navAddScore = document.getElementById('addScores');
+const boardTitle = document.getElementById('boardTitle');
+const addTitle = document.getElementById('addTitle');
+const leaderboardContainer = document.querySelector('.leaderboard-container');
+const formContainer = document.querySelector('.form-container');
+
+navLeaderboard.addEventListener('click',()=>{
+  boardTitle.classList.remove('hidden');
+  leaderboardContainer.classList.remove('hidden');
+  addTitle.classList.add('hidden');
+  formContainer.classList.add('hidden');
+});
+
+navAddScore.addEventListener('click',()=>{
+  boardTitle.classList.add('hidden');
+  leaderboardContainer.classList.add('hidden');
+  addTitle.classList.remove('hidden');
+  formContainer.classList.remove('hidden');
+});
 
 const disableButton = () => {
   submitBtn.disabled = true;
   submitBtn.innerText = 'Posting...';
 };
+
 const sendingData = async (event) => {
   event.preventDefault();
 
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
   const error = document.querySelector('small')
-  if (name.length < 20 || !isNaN(score) || score < 1000000) {
+  if (name.length > 20 || isNaN(Number(score)) || score > 1000000) {
     error.innerHTML = "please enter valid score or name";
+    submitBtn.disabled = true;
+    submitBtn.innerText = 'Try again'
+    return
   }
   await add(name, score);
   window.location.reload();
 };
-submitBtn.addEventListener('click', sendingData);
 submitBtn.addEventListener('click', disableButton);
+submitBtn.addEventListener('click', sendingData);
+
 const refreshBtn = document.querySelector('.refresh-btn');
 //  refreshing the whole page
 
