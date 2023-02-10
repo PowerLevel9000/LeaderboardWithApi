f763e29ef38931212292cb902aa43d2763d92d39
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-11 01:01:40 +0530

    linters fixed

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ const addTitle = document.getElementById('addTitle');
 const leaderboardContainer = document.querySelector('.leaderboard-container');
 const formContainer = document.querySelector('.form-container');
 
-navLeaderboard.addEventListener('click',()=>{
+navLeaderboard.addEventListener('click', () => {
   boardTitle.classList.remove('hidden');
   leaderboardContainer.classList.remove('hidden');
   addTitle.classList.add('hidden');
   formContainer.classList.add('hidden');
 });
 
-navAddScore.addEventListener('click',()=>{
+navAddScore.addEventListener('click', () => {
   boardTitle.classList.add('hidden');
   leaderboardContainer.classList.add('hidden');
   addTitle.classList.remove('hidden');
@@ -33,12 +33,12 @@ const sendingData = async (event) => {
 
   const name = document.getElementById('name').value;
   const score = document.getElementById('score').value;
-  const error = document.querySelector('small')
-  if (name.length > 20 || isNaN(Number(score)) || score > 1000000) {
-    error.innerHTML = "please enter valid score or name";
+  const error = document.querySelector('small');
+  if (name.length > 20 || Number.isNaN(Number(score)) || score > 1000000) {
+    error.innerHTML = 'please enter valid score or name';
     submitBtn.disabled = true;
-    submitBtn.innerText = 'Try again'
-    return
+    submitBtn.innerText = 'Try again';
+    return;
   }
   await add(name, score);
   window.location.reload();
@@ -56,7 +56,11 @@ refreshBtn.addEventListener('click', () => {
 const leaderboard = document.querySelector('.leaderboard');
 const displayScores = async () => {
   const waitingData = await getData();
-  const descendingData = waitingData.result.sort((a, b) => b.score - a.score).filter((a) => a.score <= 1000000000 && a.user.length <= 20);
+  const descendingData = waitingData.result.sort(
+    (a, b) => b.score - a.score,
+  ).filter(
+    (a) => a.score <= 1000000000 && a.user.length <= 20,
+  );
   descendingData.forEach((result) => {
     leaderboard.innerHTML += `
     <div class="score">${result.user.toUpperCase()}  <span>${result.score}</span></div>
