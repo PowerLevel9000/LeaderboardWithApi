3545ff942ed42bba2e4bec706118c4921bb6c070
PowerLevel9000 <116451144+PowerLevel9000@users.noreply.github.com>
2023-02-12 08:24:27 +0530

    music controls

--- src/index.js
+++ src/index.js
@@ -72,10 +72,31 @@ const displayScores = async () => {
 
 displayScores();
 const audio = document.getElementById('myAudio');
-document.getElementById('play-audio').addEventListener('click', () => {
-  audio.play();
+const playPauseBtn = document.getElementById('playPauseBtn');
+const muteBtn = document.getElementById('muteBtn');
+const volumeControl = document.getElementById('volumeControl');
+
+playPauseBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.loop = true;
+    audio.play();
+    playPauseBtn.innerHTML = 'Pause';
+  } else {
+    audio.pause();
+    playPauseBtn.innerHTML = 'Play';
+  }
 });
 
-window.addEventListener('load', () => {
-  audio.play();
+muteBtn.addEventListener('click', () => {
+  if (audio.muted) {
+    audio.muted = false;
+    muteBtn.innerHTML = 'Mute';
+  } else {
+    audio.muted = true;
+    muteBtn.innerHTML = 'Unmute';
+  }
+});
+
+volumeControl.addEventListener('input', () => {
+  audio.volume = volumeControl.value;
 });
